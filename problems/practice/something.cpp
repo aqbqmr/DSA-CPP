374f512aeb550538f176056d6b076b4203137870
Aakib Siddiki <siddiaakib2006@gmail.com>
2026-04-02 00:04:53 +0530

    chore: add practice starter file

---
+++ problems/practice/something.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // Write your C++ practice code here.
+
+    return 0;
+}
