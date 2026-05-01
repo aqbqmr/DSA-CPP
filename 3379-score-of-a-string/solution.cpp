25bae6405965e0b79344955237f1d16d3412ce09
Aakib Siddiki <siddiaakib2006@gmail.com>
2026-05-01 15:33:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9 MB (30.48%)

---
+++ 3379-score-of-a-string/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int score = 0;
+        int n = s.length();
+        for(int i = 1; i < n; i++){
+            score += abs(s[i] - s[i-1]);
+        }
+
+        // // OR
+        // for(int i = 0; i < n-1; i++){
+        //     score += abs(s[i] - s[i+1]);
+        // }
+        return score;
+    }
+};
