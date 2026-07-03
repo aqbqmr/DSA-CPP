9fd1512a94cd3618313deb29cb9e55b651847785
Aakib Siddiki <siddiaakib2006@gmail.com>
2026-07-04 01:43:50 +0530

    merge precompute things

--- manual/cpp/Hashing/hashWithHelpOfMap.cpp
+++ manual/cpp/Hashing/hashWithHelpOfMap.cpp
@@ -1,19 +1,44 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i = 0; i < n; i++){
+//         cin>>arr[i];
+//     }
+//     //pre-compute;
+//     map<int, int> mpp;
+//     for(int i=0; i<n; i++){
+//         mpp[arr[i]] += 1;
+//     }
+//     int q;
+//     cin>>q;
+//     while(q--){
+//         int numbers;
+//         cin>>numbers;
+//         //fetch;
+//         cout<<mpp[numbers]<<"";
+//     }
+//    return 0;
+// }
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
-
 int main(){
     int n;
     cin>>n;
     int arr[n];
+    map<int, int> mpp; 
     for(int i = 0; i < n; i++){
         cin>>arr[i];
-    }
-    //pre-compute;
-    map<int, int> mpp;
-    for(int i=0; i<n; i++){
         mpp[arr[i]] += 1;
     }
-
+    for(auto it: mpp){
+        cout<<it.first << "->" <<it.second<<endl;
+    }
     int q;
     cin>>q;
     while(q--){
@@ -22,4 +47,5 @@ int main(){
         //fetch;
         cout<<mpp[numbers]<<"";
     }
+    return 0;
 }
