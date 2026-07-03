8e488005cce5bfdb25a58920149dd99391cc6266
Aakib Siddiki <siddiaakib2006@gmail.com>
2026-07-03 20:03:07 +0530

    char modified hash

--- manual/cpp/Hashing/character.cpp
+++ manual/cpp/Hashing/character.cpp
@@ -2,8 +2,7 @@
 // using namespace std;
 // int main(){
 //     string s;
-//     cin >> s;
-     
+//     cin >> s; 
 //     int hash[26] = {0};
 //     for(int i = 0; i < s.size(); i++){
 //         hash[s[i]-'a']++;
