85592b23188e01e61174e1f923fa9a7e137d8cfb
Aakib Siddiki <siddiaakib2006@gmail.com>
2026-08-05 01:47:11 +0530

    Brute Force for union of two sorted arrays

--- manual/Array Data Structure/unionOfTwoSortedArray.cpp
+++ manual/Array Data Structure/unionOfTwoSortedArray.cpp
@@ -1,28 +1,30 @@
-// // Brute Force Approach....................
-// #include<bits/stdc++.h>
-// using namespace std;
-// vector <int> unionOfSortedArrays(int arr1[], int arr2[]){
-//     int n1=arr1.size();
-//     int n2 = arr2.size();
-//     set<int> st;
-//     for(int i=0; i<n1;i++){
-//         st.insert(arr1[i]);
-//     }
-//     for(int i=0; i<n2;i++){
-//         st.insert(arr2[i]);
-//     }
-//     vector<int> temp;
-//     for(auto it: st){
-//         temp.push_back(it);
-//     }
-// }
-// int main(){
-//     int arr1[] = {1,4,5,6,2,3,4,5};
-//     int arr2[] = {1,3,5,6,7,3,2};
-//     unionOfSortedArrays(arr1, arr2);
-// }
-
-
+// Brute Force Approach....................
+#include<bits/stdc++.h>
+using namespace std;
+vector <int> unionOfSortedArrays(int arr1[], int n1, int arr2[], int n2){
+    set<int> st;
+    for(int i=0; i<n1;i++){
+        st.insert(arr1[i]);
+    }
+    for(int i=0; i<n2;i++){
+        st.insert(arr2[i]);
+    }
+    vector<int> temp;
+    for(auto it : st){
+        temp.push_back(it);
+    }
+    return temp;
+}
+int main(){
+    int arr1[] = {1,4,5,6,2,3,4,5};
+    int arr2[] = {1,3,5,6,7,3,2};
+    int n1 = sizeof(arr1) / sizeof(arr1[0]);
+    int n2 = sizeof(arr2) / sizeof(arr2[0]);
+    vector<int> result = unionOfSortedArrays(arr1, n1, arr2, n2);
+    for(auto it : result){
+        cout<<it<<" ";
+    }
+}
 
 
 // Optimal Approach............................
