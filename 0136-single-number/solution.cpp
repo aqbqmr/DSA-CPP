003227ff3b1373572b718b906ed0deffed5ec683
Aakib Siddiki <siddiaakib2006@gmail.com>
2026-08-19 06:28:54 +0000

    [LeetCode Sync] Runtime - 11 ms (13.47%), Memory - 23.8 MB (12.41%)

--- 0136-single-number/solution.cpp
+++ 0136-single-number/solution.cpp
@@ -1,20 +1,35 @@
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         int n = nums.size();
+//         for(int i = 0; i < n; i++){
+//             int count = 0;
+//             for(int j = 0; j < n; j++){
+//                 if(nums[i] == nums[j]){
+//                     count++;
+//                 }
+//             }
+//             if(count==1) return nums[i]; 
+//         }
+//         return -1;
+//     }
+// };
+
+
+//optimal approach........
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
-        for(int i = 0; i < n; i++){
-            int count = 0;
-            for(int j = 0; j < n; j++){
-                if(nums[i] == nums[j]){
-                    count++;
-                }
+        unordered_map<int, int>hash;
+        for(auto num : nums){
+            hash[num]++;
+        }
+        for(auto x: hash){
+            if(x.second == 1){
+                return x.first;
             }
-            if(count==1) return nums[i]; 
         }
         return -1;
     }
 };
-
-
-// i need to comeback at this question
-
