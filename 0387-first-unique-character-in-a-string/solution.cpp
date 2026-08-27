0e86437c9b91c052a090fdc96dd80c405754a2ab
Aakib Siddiki <siddiaakib2006@gmail.com>
2026-08-27 10:59:26 +0000

    [LeetCode Sync] Runtime - 10 ms (51.89%), Memory - 14.9 MB (20.68%)

--- 0387-first-unique-character-in-a-string/solution.cpp
+++ 0387-first-unique-character-in-a-string/solution.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char, int> hash;
-        for(int i = 0; i < s.size(); i++){
-            hash[s[i]]++;
+         int n = s.size();
+        unordered_map<char, int> mpp;
+        for(int i = 0; i < n; i++){
+            mpp[s[i]]++;
         }
-        for(int i = 0; i < s.size(); i++){
-            if(hash[s[i]] == 1) return i;
+        for(int i = 0; i < n; i++){
+            if(mpp[s[i]] == 1) return i;
         }
         return -1;
     }
