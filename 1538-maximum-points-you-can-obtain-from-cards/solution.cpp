436717336c035521444fc5b0da087644bf317942
Aakib Siddiki <siddiaakib2006@gmail.com>
2026-09-18 20:19:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 46.1 MB (94.63%)

--- 1538-maximum-points-you-can-obtain-from-cards/solution.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/solution.cpp
@@ -1,20 +1,46 @@
+// class Solution {
+// public:
+//     int maxScore(vector<int>& nums, int k) {
+//         int lsum = 0;
+//         int rsum = 0;
+//         int maxSum = 0;
+//         for(int i = 0; i <= k-1; i++){
+//             lsum += nums[i];
+//         }
+//             maxSum = lsum;
+//             int rIdx = nums.size()-1;
+//             for(int i = k-1; i >= 0; i--){
+//                 lsum -= nums[i];
+//                 rsum += nums[rIdx];
+//                 rIdx --;
+//                 maxSum = max(maxSum, lsum + rsum);
+//             }
+//         return maxSum;
+//     }
+// };
+
+
+
 class Solution {
 public:
     int maxScore(vector<int>& nums, int k) {
         int lsum = 0;
         int rsum = 0;
         int maxSum = 0;
-        for(int i = 0; i <= k-1; i++){
+        int n = nums.size();
+        for(int i = 0; i < k; i++){
             lsum += nums[i];
         }
-            maxSum = lsum;
-            int rIdx = nums.size()-1;
-            for(int i = k-1; i >= 0; i--){
-                lsum -= nums[i];
-                rsum += nums[rIdx];
-                rIdx --;
-                maxSum = max(maxSum, lsum + rsum);
-            }
+        maxSum = lsum;
+        int l = k - 1;
+        int r = n - 1;
+        while(l >= 0){
+            lsum -= nums[l];
+            rsum += nums[r];
+            l--;
+            r--;
+            maxSum = max(maxSum, lsum+rsum);
+        }
         return maxSum;
     }
 };
